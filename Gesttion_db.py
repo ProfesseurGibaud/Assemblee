8d03a4491d5feba271e18bbece228c29126be62c
Professeur Gibaud <mail@professeurgibaud.ovh>
2021-06-27 17:53:55 +0200

    Mise à jour de la BDD done

--- Gesttion_db.py
+++ Gesttion_db.py
@@ -2,8 +2,9 @@ import sqlite3
 from lxml import etree
 import os
 
-
-
+# Variables globale pour mettre dans la base les mandats et les groupes des élus
+dico_elu_groupe = {}
+dico_elu_mandat = {}
 
 conn = sqlite3.connect('data_full.db')
 cursor = conn.cursor()
@@ -21,6 +22,7 @@ def Ouverture_organe():
         cursor.execute("""INSERT INTO Organe(uid,codeType,libelle) VALUES(?,?,?)""", (uid,codeType,libelle))
 
 def Ouverture_depute(): #Je dois le lancer pour mettre à jour Bd
+    global dico_elu_groupe, dico_elu_mandat
     liste_depute = os.listdir("Depute")
     print(len(liste_depute))
     for depute in liste_depute:
@@ -36,13 +38,198 @@ def Ouverture_depute(): #Je dois le lancer pour mettre à jour Bd
         prenom = etat_civil_id.getchildren()[1].text
         nom = etat_civil_id.getchildren()[2].text
         date_naissance = etat_civil.getchildren()[1][0].text
-        profession = root_children[2].getchildren()[0]
-        catSocPro = root_children[2].getchildren()[1].getchildren()[0]
-        famSocPro = root_children[2].getchildren()[1].getchildren()[1]
-        cursor.execute("""INSERT INTO Depute(uid,civ,prenom,nom,dateNais,profession,catSocPro,famSocPro) VALUES(?,?,?,?,?,?,?,?)""", (uid,civ,prenom,nom,date_naissance,profession,catSocPro,famSocPro))
+        profession = root_children[2].getchildren()[0].text
+        catSocPro = root_children[2].getchildren()[1].getchildren()[0].text
+        famSocPro = root_children[2].getchildren()[1].getchildren()[1].text
+        if uid in dico_elu_groupe.keys():
+            groupeRef = dico_elu_groupe[uid]
+            mandat = dico_elu_mandat[uid]
+        else :
+            groupeRef = ""
+            mandat = ""
+        cursor.execute("""INSERT INTO Elu(uid,civ,prenom,nom,dateNais
+                        ,profession,catSocPro,famSocPro,groupeRef,mandat)
+                        VALUES(?,?,?,?,?,?,?,?,?,?)""",
+                        (uid,civ,prenom,nom,date_naissance,profession,
+                        catSocPro,famSocPro,groupeRef,mandat))
 
 
 #Prof = root_children[2].getchildren()
+
+
+liste_scrutin = os.listdir("Scrutin")
+for scrutin in liste_scrutin:
+    # On prépare le lien fichier.
+    LienFichier = os.path.join("Scrutin",scrutin)
+
+    tree = etree.parse(LienFichier)
+    root = tree.getroot()
+    root_children = root.getchildren()
+
+    uid = root_children[0].text
+    organeRef = root_children[2].text
+    legislature = int(root_children[3].text)
+    sessionRef =  root_children[4].text
+    seanceRef = root_children[5].text
+    dateScrutin = root_children[6].text
+    quantiemeJourSeance = int(root_children[7].text)
+    sort = root_children[9].getchildren()[0].text
+    titre = root_children[10].text
+    demandeur = root_children[11].getchildren()[0].text
+    objet = root_children[12].getchildren()[0].text
+    synthese_vote_children = root_children[14].getchildren()
+
+    # On récupère les entiers (suffrages, votants etc...)
+    nombreVotants = int(synthese_vote_children[0].text)
+    suffragesExprimes = int(synthese_vote_children[1].text)
+    nbrSuffragesRequis = int(synthese_vote_children[2].text)
+    nonVotants = int(synthese_vote_children[4].getchildren()[0].text)
+    pour = int(synthese_vote_children[4].getchildren()[1].text)
+    contre = int(synthese_vote_children[4].getchildren()[2].text)
+    abstentions = int(synthese_vote_children[4].getchildren()[3].text)
+    nonVotantsVolontaires = int(synthese_vote_children[4].getchildren()[4].text)
+
+    # On passe aux votes nominatifs
+    groupe_votants = root_children[15].getchildren()[0].getchildren()[1].getchildren()  #groupe_votants représentent les groupes politiques
+
+    dico_vote_liste = {"Non Votant" : "", "Pour" : "", "Contre":"","Abstention":""}
+
+    listeUidNonVotants = ""
+    listeUidPours = ""
+    listeUidContres = ""
+    listeUidAbstentions = ""
+
+
+    for groupe in groupe_votants:
+        groupeRef = groupe[0].text
+        groupe_vote_nom = groupe[2].getchildren()[2]
+
+        groupe_dico_vote_nom = {"Non Votant" : groupe_vote_nom[0], "Pour" : groupe_vote_nom[1], "Contre" : groupe_vote_nom[2], "Abstention" : groupe_vote_nom[3]}
+
+        for vote,liste in groupe_dico_vote_nom.items():
+            print(vote)
+            for elu in liste: #Attention elu est un objet, acteurRef est le nom (string)
+                acteurRef = elu.getchildren()[0].text
+                print(acteurRef)
+                mandatRef = elu.getchildren()[1].text
+                if acteurRef not in dico_elu_groupe:
+                    dico_elu_groupe[acteurRef] = groupeRef
+                elif not groupeRef in dico_elu_groupe[acteurRef]:
+                    dico_elu_groupe[acteurRef] += "/" + groupeRef
+                if elu not in dico_elu_mandat:
+                    dico_elu_mandat[acteurRef] = groupeRef
+                elif not groupeRef in dico_elu_mandat[acteurRef]:
+                    dico_elu_mandat[acteurRef] += "/" + mandatRef
+                dico_vote_liste[vote] += acteurRef + "/"
+
+
+
+
+    listeUidNonVotants = dico_vote_liste["Non Votant"][:-1]
+    listeUidPours = dico_vote_liste["Pour"][:-1]
+    listeUidContres = dico_vote_liste["Contre"][:-1]
+    listeUidAbstentions = dico_vote_liste["Abstention"][:-1]
+    cursor.execute("""INSERT INTO Scrutin
+                    (uid,organeRef,legislature,sessionRef,
+                    seanceRef,dateScrutin,quantiemeJourSeance,
+                    sort,titre,demandeur,objet,nombreVotants,
+                    suffragesExprimes,nbrSuffragesRequis,
+                    nonVotants,pour,contre,abstentions,
+                    nonVotantsVolontaires,
+                    listeUidNonVotants,listeUidPours,
+                    listeUidContres,listeUidAbstentions)
+                    VALUES(?,?,?,?,?,?,?,?,?,?,?,
+                            ?,?,?,?,?,?,?,?,?,?,?,?)""",
+                    (uid,organeRef,legislature,sessionRef,
+                    seanceRef,dateScrutin,quantiemeJourSeance,
+                    sort,titre,demandeur,objet,nombreVotants,
+                    suffragesExprimes,nbrSuffragesRequis,
+                    nonVotants,pour,contre,abstentions,
+                    nonVotantsVolontaires,
+                    listeUidNonVotants,listeUidPours,
+                    listeUidContres,listeUidAbstentions))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+groupe = groupe_votants[0]
+groupeRef = groupe[0].text
+groupe_vote_nom = groupe[2].getchildren()[2]
+
+groupe_dico_vote_nom = {"Non Votant" : groupe_vote_nom[0], "Pour" : groupe_vote_nom[1], "Contre" : groupe_vote_nom[2], "Abstention" : groupe_vote_nom[3]}
+
+for vote,liste in groupe_dico_vote_nom.items():
+    print(vote)
+    for elu in liste:
+        acteurRef = elu.getchildren()[0].text
+        print(acteurRef)
+        mandatRef = elu.getchildren()[1].text
+        if elu not in dico_elu_groupe:
+            dico_elu_groupe[elu] = groupeRef
+        elif not groupeRef in dico_elu_groupe[elu]:
+            dico_elu_groupe[elu] += "/" + groupeRef
+        if elu not in dico_elu_mandat:
+            dico_elu_mandat[elu] = groupeRef
+        elif not groupeRef in dico_elu_mandat[elu]:
+            dico_elu_mandat[elu] += "/" + mandatRef
+        dico_vote_liste[vote] += acteurRef + "/"
+
+
+
+
+listeUidNonVotants = dico_vote_liste["Non Votant"][:-1]
+listeUidPours = dico_vote_liste["Pour"][:-1]
+listeUidContres = dico_vote_liste["Contre"][:-1]
+listeUidAbstentions = dico_vote_liste["Abstention"][:-1]
+
+"""
+
+
+
+"""
+
+Pour la gestion des groupes politiques à l'AN : on va faire une liste dans groupe Ref pour les elu.
+
+On fait un Dico avec tous les élus qui ont voté quelque chose dans lequel on ajoute "/" au début. Puis avant la MAJ dans la bd on enlève le premier "/".
+
+
+Ordre des entrées dans la BD :
+1) Organes
+2) Srutins (pour que le dico Elu-> Groupe soit prêt)
+3) Elu
+
+
+
+"""
 
 
 def Fin():
@@ -50,7 +237,6 @@ def Fin():
     conn.close()
 
 
+#LienFichier = os.path.join("Depute","PA267285.xml")
 
-
-
-#os.system("explorer " + LienFichier)
+os.system("explorer " + LienFichier)
